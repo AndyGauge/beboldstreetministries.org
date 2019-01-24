94a79fafdcc719c38cb984790c0328dd595c9385
Andy Gauge <andygauge@gmail.com>
2019-01-23 16:51:28 -0800

    first attempt at give

---
+++ precache-manifest.42dff01cefcb9858d71918f627b321c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e893756ea1ba48293dcc",
+    "url": "/static/js/main.e893756e.chunk.js"
+  },
+  {
+    "revision": "febf8d820d7528079af7",
+    "url": "/static/js/1.febf8d82.chunk.js"
+  },
+  {
+    "revision": "e893756ea1ba48293dcc",
+    "url": "/static/css/main.2f720b0a.chunk.css"
+  },
+  {
+    "revision": "febf8d820d7528079af7",
+    "url": "/static/css/1.58cd1d92.chunk.css"
+  },
+  {
+    "revision": "95629ac55b256c8eb0737a7eb9545a75",
+    "url": "/index.html"
+  }
+];
